b0030319fce473ff88c604e18e20176f9661dfe8
HohShenYien <hohshenyien@gmail.com>
2023-06-12 20:28:12 +0800

    Added some docs

--- frontend/src/utils/hooks/useServerAction.ts
+++ frontend/src/utils/hooks/useServerAction.ts
@@ -4,12 +4,29 @@ type UseServerActionType = <T extends (...args: any[]) => any>(
   serverAction: T
 ) =>
   | {
+      /** The function caller */
       invoke: (...args: Parameters<T>) => void;
+      /** The data returned by the server action */
+      data: Awaited<ReturnType<T>> | null;
+      /** If the server action is running in the server currently */
+      isLoading: boolean;
+      /** Is the data populated: Has the server action returns? */
+      isSuccess: boolean;
     } & (
-      | { data: Awaited<ReturnType<T>>; isLoading: false; isSuccess: true }
+      | {
+          data: Awaited<ReturnType<T>>;
+          isLoading: false;
+          isSuccess: true;
+        }
       | { data: null; isLoading: boolean; isSuccess: false }
     );
 
+/**
+ * Use this hook whenever you're calling server action for simplicity
+ *
+ * @param serverAction The server action function from the file with "use server"
+ *
+ */
 // @ts-ignore
 const useServerAction: UseServerActionType = <
   T extends (...args: any[]) => any

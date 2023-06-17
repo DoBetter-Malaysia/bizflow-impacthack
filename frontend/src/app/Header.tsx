5d31165184115943b11e6cb078a7fae2ed106b4a
Khor Zhen Win <78037711+khorzhenwin@users.noreply.github.com>
2023-06-18 04:44:15 +0800

    added integrations into header w/ integrations mock data

--- frontend/src/app/Header.tsx
+++ frontend/src/app/Header.tsx
@@ -2,7 +2,8 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { FiArrowRight } from "react-icons/fi";
+import { Select } from "@mantine/core";
+import { FiGlobe } from "react-icons/fi";
 
 const Header = () => {
   return (
@@ -19,19 +20,32 @@ const Header = () => {
           <h1 className="text-3xl font-bold">BizFlow</h1>
         </div>
       </Link>
-      <Link href={"/consult"}>
-        <div className="flex items-center justify-center gap-0 rounded-md bg-[#004be0] px-4 py-2 align-middle text-white">
-          <Image
-            src="/logo_icon_split.png"
-            alt="logo"
-            width={40}
-            height={40}
-            className="object-cover"
-          />
-          <p>FlowAI</p>
-          {/* <FiArrowRight size={30} className="font-bold text-[#004be0]" /> */}
-        </div>
-      </Link>
+      <div className="flex items-center gap-8">
+        <Select
+          placeholder="English"
+          data={["English", "Bahasa Malaysia", "Mandarin", "Tamil"]}
+          icon={<FiGlobe size="1rem" />}
+        />
+        <Link href={"/integrations"}>
+          <div className="flex flex-col gap-0 font-semibold">
+            <div className="py-auto text-center">
+              <p className="text-[#004be0]">Integrations</p>
+            </div>
+          </div>
+        </Link>
+        <Link href={"/consult"}>
+          <div className="flex items-center justify-center gap-0 rounded-md bg-[#004be0] px-4 py-2 align-middle text-white">
+            <Image
+              src="/logo_icon_split.png"
+              alt="logo"
+              width={40}
+              height={40}
+              className="object-cover"
+            />
+            <p>FlowAI</p>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };

841f32929d9470edfd71f0a093b29c68c09e92a7
Khor Zhen Win <78037711+khorzhenwin@users.noreply.github.com>
2023-06-21 15:56:54 +0800

    draft for chart

--- frontend/src/components/cards/RankingCard.tsx
+++ frontend/src/components/cards/RankingCard.tsx
@@ -1,18 +1,32 @@
 import React from "react";
+import { clsx } from "@mantine/core";
 
-export type RankType = "Industry" | "Platform";
 export interface RankingCardProps {
   rank: number;
   type: string;
-  industry?: string;
+  isRank: boolean;
 }
 
-const RankingCard = ({ rank, type, industry }: RankingCardProps) => {
+const RankingCard = ({ rank, type, isRank }: RankingCardProps) => {
   return (
-    <div className="flex h-40 w-full items-center rounded-lg bg-white shadow-md">
-      <div className="flex justify-start">
-        <div className="h-28 w-3 rounded-br-lg rounded-tr-lg bg-[#004be0]" />
-        <div className="pl-8"></div>
+    <div className="flex h-28 w-full items-center rounded-lg bg-white shadow-md">
+      <div className="flex items-center justify-start">
+        <div
+          className={clsx(
+            "h-16 w-3 rounded-br-lg rounded-tr-lg",
+            { "bg-[#38a5ff]": isRank },
+            { "bg-[#004be0]": !isRank }
+          )}
+        />
+        <div className="flex flex-col space-y-2 pl-8">
+          <div className=" flex flex-col text-black">
+            <div className="flex items-end">
+              {isRank && <span className="mr-2 text-sm">#</span>}
+              <span className="text-3xl font-semibold">{rank}</span>
+            </div>
+            <span className="text-sm text-gray-600">{type}</span>
+          </div>
+        </div>
       </div>
     </div>
   );
